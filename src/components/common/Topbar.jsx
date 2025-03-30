6170a4e08d25bfcb6ab009cba3bc643cae46fcad
support@prolianceltd.com <support@prolianceltd.com>
2025-03-30 15:39:23 +0100

    yeah

--- src/components/common/Topbar.jsx
+++ src/components/common/Topbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FiBell, FiSearch, FiUser, FiHome } from 'react-icons/fi';
+import './Topbar.css';
 
 const Topbar = () => {
   // const { currentUser } = useAuth();
